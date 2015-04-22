6324448f1e966e2f6610bed6575be6640638fe9d
増田貴志 <xellos00195@gmail.com>
2015-04-22 22:16:13 +0900

    生徒さん登録フォーム　性別ラジオボタン修正

--- js/controlPage.js
+++ js/controlPage.js
@@ -227,9 +227,9 @@ function createFormData(form){
 				&& $(this).index('[name="' + name + '"]') != 0 || name === void(0)){
 			//何もしない。
 			//ラジオボタンかつieであれば
-			} else if(type == 'radio' || uaName.indexOf('ie') != -1){
+			} else if(type == 'radio' && uaName.indexOf('ie') != -1){
 				//配列にチェックボックスの値をgetAttributeNodeメソッドを使い格納していく。
-				val = this.getAttributeNode('value').text;	//IEでの値を取得するやり方を使う。
+				val = this.getAttributeNode('value').value;	//IEでの値を取得するやり方を使う。
 			//それ以外であれば
 			} else {
 				//name属性がnameであれば
