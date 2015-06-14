ffe23a414815f70cd8d8dd9673140f322d0473f4
山本翼 <bnp1125@gmail.com>
2015-06-14 22:04:26 +0900

    会員トップ確認ダイアログの追加

--- js/dailyClasses.js
+++ js/dailyClasses.js
@@ -31,7 +31,7 @@ var fullHouse = "満席";
 * @param mixed timeTableRows 授業情報を(1時間割分全部)格納した連想配列
 * @return int timeTableStudents 時間割全体の予約人数
 */ 
-function getTotalStudentsOfTimeTable(timeTableRows) {
+var getTotalStudentsOfTimeTable = function(timeTableRows) {
 	var students = 0;
 	for (var key in timeTableRows) {
 		students += timeTableRows[key].students;
@@ -215,6 +215,34 @@ function buildHourFromTo(rowData) {
 }
 
 /* 
+ * 関数名:sumCost
+ * 概要  :料金を求める
+ * 引数  :rowData:連想配列名。テーブルから取り出した値
+ 		:columnName:テーブルの列名(連想配列のキー名)
+ * 返却値  :resultString: 後ろから3文字を削除した結果の文字列
+ * 用途例:11:00:00 となっているところの後ろの「:00」を削除する
+ * 作成者:T.Yamamoto
+ * 作成日:2015.06.14
+ */
+function sumCost(rowData, rowNumber) {
+	// 合計の結果変数を作る
+	var sumResult = 0;
+	// 合計を求める配列を変数に入れる
+	// var arrayValue = rowData[rowNumber];
+
+	// key名にcostがつくものがあれば合計の変数に足す
+	for (var key in rowData) {
+		// key名にcostがつくものがあれば合計の変数に値を足す
+		if(key.match(/cost/)) {
+			// 合計の変数に連想配列の値を数字に変換して合計する
+			 sumResult += Number(rowData[key]);
+		}
+	}
+	// 合計の変数を返す
+	return sumResult;
+}
+
+/* 
  * 関数名:backThreeStringDelete
  * 概要  :第一引数の配列から第二引数のキーの値について、その値の後ろから3文字を削除する
  * 引数  :rowData:連想配列名。テーブルから取り出した値
@@ -234,6 +262,25 @@ function backThreeStringDelete(rowData, columnName) {
 }
 
 /* 
+ * 関数名:frontTwoStringDelete
+ * 概要  :第一引数の配列から第二引数のキーの値について、その値の前から2文字を削除する
+ * 引数  :rowData:連想配列名。テーブルから取り出した値
+ 		:columnName:テーブルの列名(連想配列のキー名)
+ * 返却値  :resultString: 後ろから3文字を削除した結果の文字列
+ * 用途例:11:00:00 となっているところの後ろの「:00」を削除する
+ * 作成者:T.Yamamoto
+ * 作成日:2015.06.13
+ */
+function frontTwoStringDelete(rowData, columnName) {
+	// 第一引数の連想配列に対して、第二引数の値を返却結果変数に入れる
+	var resultString = rowData[columnName];
+	// 値の前から2文字削除した文字列を結果の変数に入れる
+	resultString = resultString.substr(2, (resultString.length));
+	// 結果の文字列を返す
+	return resultString;
+}
+
+/* 
  * 関数名:backThreeStringDelete
  * 概要  :第一引数の配列から第二引数のキーの値について、その値の後ろから3文字を削除する
  * 引数  :rowData:連想配列名。テーブルから取り出した値
@@ -243,11 +290,41 @@ function backThreeStringDelete(rowData, columnName) {
  * 作成者:T.Yamamoto
  * 作成日:2015.06.13
  */
-function buildHourFromToEx(rowData) {
+var buildHourFromToEx = function(rowData) {
 	var start_time = backThreeStringDelete(rowData, 'start_time');
 	var end_time = backThreeStringDelete(rowData, 'end_time');
 	var result = start_time + '-' + end_time;
 	return result;
+};
+
+/* 
+ * 関数名:allDateTime
+ * 概要  :年月日と開始時間、終了時間を返す
+ * 引数  :rowData:連想配列名。テーブルから取り出した値
+ 		:columnName:テーブルの列名(連想配列のキー名)
+ * 返却値  :resultString: 後ろから3文字を削除した結果の文字列
+ * 用途例:11:00:00 となっているところの後ろの「:00」を削除する
+ * 作成者:T.Yamamoto
+ * 作成日:2015.06.13
+ */
+function　allDateTime(rowData) {
+	var result;
+	result = frontTwoStringDelete(rowData, 'lesson_date');
+	result += ' ';
+	result += buildHourFromToEx(rowData);
+	return result;
+};
+
+/* 
+ * 関数名:callFunction
+ * 概要  :関数を呼ぶための関数
+ * 引数  :なし
+ * 返却値  :なし
+ * 作成者:T.Yamamoto
+ * 作成日:2015.06.14
+ */
+function callFunction() {
+	return 
 }
 
 /* 
@@ -261,7 +338,7 @@ function buildHourFromToEx(rowData) {
  * 作成者:T.Yamamoto
  * 作成日:2015.06.13
  */
-function tableColumnValueInput(tableName, columnNumber, array, functionName) {
+function tableColumnValueInput(tableName, columnNumber, array, func) {
 	// カウンターを作る
 	var counter = 1;
 	// 結果の変数をあらかじめ作る
@@ -270,12 +347,94 @@ function tableColumnValueInput(tableName, columnNumber, array, functionName) {
 	$.each(array, function() {
 		// テーブルの値に入る連想配列(テーブルの値一覧)を変数に入れる
 		var arrayValue = array[counter];
-		// テーブルの値から取り出し、編集した結果を変数resultに入れる
-		result = buildHourFromToEx(arrayValue);
+		// テーブルの値から取り出し、編集した結果を変数resulstに入れる
+		result = func(arrayValue);
 		// 第一引数のテーブルの行番号と列番号を指定して、結果のテキストをセルに入れる
 		$(tableName + ' tr:eq(' + counter + ') td').eq(columnNumber).text(result);
 		// 関数に1を足す
 		counter++;
 	});
 }
+
+/* 
+ * 関数名:reservedLessonValueInput
+ * 概要  :予約テーブルについてデータベースから取り出した値を固定値で入れる
+ * 引数  :
+ * 返却値  :なし
+ * 作成者:T.Yamamoto
+ * 作成日:2015.06.13
+ */
+function reservedLessonValueInput(tableName, array) {
+	// カウンターを作る
+	var counter = 0;
+	// テーブルの行番号
+	var rowNumber = 1;
+	// 結果の変数をあらかじめ作る
+	var result;
+	// テーブルのすべての行に対してループで値を入れる
+	$.each(array, function() {
+		// テーブルの値に入る連想配列(テーブルの値一覧)を変数に入れる
+		var arrayValue = array[counter];
+		// 開始日時と終了時刻を組み合わせた値を入れる
+		resultColumn0 = buildHourFromToEx(arrayValue);
+		resultColumn5 = getTotalStudentsOfTimeTable(arrayValue);
+		// 料金を求める
+		resultColumn3 = sumCost(arrayValue, counter);
+		// 残席を求める
+		// result4 = getRestMark(arrayValue, arrayValue['pre_order_days']);
+		// 開始日時と終了時間を合わせてテーブルの最初のカラムに値を入れる
+		$(tableName + ' tr:eq(' + rowNumber + ') td').eq(0).text(resultColumn0);
+		// 料金の表示を正規の表示にする
+		 $(tableName + ' tr:eq(' + rowNumber + ') td').eq(3).text(resultColumn3);
+		// 残席の表示を正規の表示にする
+		// $(tableName + ' tr:eq(' + rowNumber + ') td').eq(4).text(result4);
+		// 残席の表示を正規の表示にする
+		$(tableName + ' tr:eq(' + rowNumber + ') td').eq(5).text(resultColumn5);
+		rowNumber++;
+		// カウンタ変数をインクリメントする
+		counter++;
+	});
+}
+
+/* 
+ * 関数名:reservedLessonValueInput
+ * 概要  :予約テーブルについてデータベースから取り出した値を固定値で入れる
+ * 引数  :
+ * 返却値  :なし
+ * 作成者:T.Yamamoto
+ * 作成日:2015.06.13
+ */
+function reservedValueInput(tableName, array) {
+	// カウンターを作る
+	var counter = 0;
+	// テーブルの行番号
+	var rowNumber = 1;
+	// 結果の変数をあらかじめ作る
+	var result;
+	// テーブルのすべての行に対してループで値を入れる
+	$.each(array, function() {
+		// テーブルの値に入る連想配列(テーブルの値一覧)を変数に入れる
+		var arrayValue = array[counter];
+		// 開始日時と終了時刻を組み合わせた値を入れる
+		resultColumn0 = allDateTime(arrayValue);
+		// resultColumn5 = getTotalStudentsOfTimeTable(arrayValue);
+		// 料金を求める
+		resultColumn4 = sumCost(arrayValue, counter);
+		// 残席を求める
+		// result4 = getRestMark(arrayValue, arrayValue['pre_order_days']);
+		// 開始日時と終了時間を合わせてテーブルの最初のカラムに値を入れる
+		$(tableName + ' tr:eq(' + rowNumber + ') td').eq(0).text(resultColumn0);
+		// 料金の表示を正規の表示にする
+		$(tableName + ' tr:eq(' + rowNumber + ') td').eq(4).text(resultColumn4);
+		// 残席の表示を正規の表示にする
+		// $(tableName + ' tr:eq(' + rowNumber + ') td').eq(4).text(result4);
+		// 残席の表示を正規の表示にする
+		// $(tableName + ' tr:eq(' + rowNumber + ') td').eq(5).text(resultColumn5);
+		rowNumber++;
+		// カウンタ変数をインクリメントする
+		counter++;
+	});
+}
+
+
 
