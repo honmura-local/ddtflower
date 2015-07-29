84b95f447922e5195b7fc45d3a478d94fb2f1749
user <user@user-no-MacBook-Pro.local>
2015-07-29 11:46:23 +0900

    環境ごとにDBの設定をかえれるようにした

---
+++ php/DbDefine.php
@@ -0,0 +1,8 @@
+<?php
+
+// データベースに接続するための値を定数として宣言する
+define('DSN', 'mysql:host=localhost;dbname=ddthink-com00006');  // データソースネーム(ホスト名、DB名)
+define('DB_USER', 'ddt42625');                                  // データベースユーザ
+define('DB_PASSWORD', 'nR6Cf84S');                              // データベースパスワード
+define('DB_HOST', 'localhost');                                 // データベースホスト
+define('DB_DATABASE', 'ddthink-com00006');                      // データベース
