2b1fc9c2b649712a5eeeef361a1a46eb897bb362
山本翼 <bnp1125@gmail.com>
2015-08-01 13:45:39 +0900

    受講済みテーブルページング対応

--- js/createLittleContent.js
+++ js/createLittleContent.js
@@ -3658,10 +3658,8 @@ function createMemberFinishedLessonContent() {
 	creator.getJsonFile(URL_GET_JSON_ARRAY_PHP, creator.json['finishedLessonTable'], 'finishedLessonTable');
 	//ページング機能付きでメルマガテーブルを作る
 	creator.outputNumberingTag('finishedLessonTable', 1, 4, 1, 10, '.finishedLessonTableOutside');
-	// console.log(creator.json.finishedLessonTable);
 	//予約中テーブルのテーブルの値をしかるべき値にする
 	lessonTableValueInput('.finishedLessonTable', creator.json.finishedLessonTable.table, 'callMemberLessonValue');
-	console.log(creator.json.finishedLessonTable);
 
 	//ページング機能付きで受講済みテーブルを表示する(レコードの表示数が15、ページングの最大値が5)
 	// tablePaging('finishedLessonTable', 15, 6);
@@ -3669,6 +3667,8 @@ function createMemberFinishedLessonContent() {
 	setSelectboxValue('.selectThemebox');
 	//絞り込みボタン機能を実装する
 	reloadTableTriggerEvent('#finishedLesson .selectThemebox', CHANGE, 'finishedLessonTable');
+	//ページング後の処理を登録する
+	finshedLessonTableAfterPaging()
 }
 
 /* 
@@ -4007,7 +4007,7 @@ function cutString(cutTargetSelector, cutCount) {
  * 作成日:2015.07.22
  */
 function sendMail(mailInfoArray, mailSubject, mailContent) {
-		//Ajax通信を行う
+	//Ajax通信を行う
 	$.ajax({
 		url: 'php/mailSend.php',		//メールを送信するためのphpをコールして処理を開始する
 		//メール情報を送信する
@@ -4321,6 +4321,7 @@ function updateMyGalleryPhotoData() {
 		}
 	});
 }
+
 /* 
  * 関数名:myGalleryDbUpdate
  * 概要  :会員マイギャラリー画面でチェックボックスにチェックが入っているコンテンツについてdbデータを更新する(updateまたはdeleteを行う)
@@ -4345,4 +4346,38 @@ function myGalleryDbUpdate(sendQueryKey, checkContentCount) {
 		$('.myPhoto').eq(deleteContentNumber).remove();
 		// console.log(deleteContentNumber);
 	}
+}
+
+/* 
+ * 関数名:finshedLessonTableAfterPaging
+ * 概要  :会員トップ、受講済みテーブルでページングボタンがクリックされた時にテーブルの値を置換する処理を行う
+ * 引数  :なし
+ * 返却値  :なし
+ * 作成者:T.Yamamoto
+ * 作成日:2015.07.30
+ */
+function finshedLessonTableAfterPaging() {
+	//ページングがクリックされた時のイベントを登録する
+	$(STR_BODY).on(CLICK, '.finishedLessonPagingArea .numbering li', function() {
+		//受講済みテーブルを編集が終わるまで表示しなくする
+		$('.finishedLessonTable').hide();
+		//時間差で表現するためにsetTimeOutを使う
+		setTimeout(function(){
+			//ページングの処理を行う件数を取得するためにページングの現在のページを取得する
+			var nowPageNumber = NUMBER($('.select').text());
+			//テーブルの値を編集するループを開始する値を取得する
+			var loopStartCount = nowPageNumber * 10;
+			//テーブルの値を編集するループを終了する値を取得する
+			var loopEndCount = nowPageNumber * 10 + 9;
+			//テーブルのデータを取得する
+			var tableRow = creator.json.finshedLessonTable.table;
+			//ループで受講済みテーブルを編集する
+			for(loopStartCount; loopStartCount<=loopEndCount; loopStartCount++) {
+				//テーブルの値を置換する
+				callMemberLessonValue('.finishedLessonTable', tableRow, loopStartCount, loopStartCount);
+			}
+			//受講済みテーブルを表示する
+			$('.finishedLessonTable').show();
+		},1);
+	});
 }
