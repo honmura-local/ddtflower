b16bfe8f26d544337092dc61b7315801c27ab4e6
増田貴志 <xellos00195@gmail.com>
2016-02-13 18:38:26 +0900

    受講承認.sql対応

--- document/sql/受講承認.sql
+++ document/sql/受講承認.sql
@@ -86,3 +86,96 @@ SELECT
 	,id AS commodity_key
 FROM
 	commodity_inf
+	
+	
+#受講承認
+#受講承認対象の一覧取得
+delimiter $$
+CREATE PROCEDURE getLecturePermit()
+BEGIN
+CREATE TEMPORARY TABLE 
+	tmp_lecture_permit AS
+SELECT 
+	'' AS columnCheckbox
+	,time_table_day.id AS time_table_key 
+	,time_table_day.lesson_date AS lesson_date 
+	,classwork.order_students AS order_students    
+	,classwork.lesson_key AS lesson_key
+	,start_time 
+	,end_time 
+	,lesson_name 
+	,user_name 
+	,user_classwork.stage_no AS stage_no 
+	,user_classwork.level_no AS level_no 
+	,user_classwork_cost 
+	,user_inf.get_point AS get_point 
+	,user_classwork.id AS user_classwork_key 
+	,user_inf.id AS user_key 
+	,timetable_inf.school_key AS school_key
+	,classwork.order_students
+FROM 
+	time_table_day 
+INNER JOIN 
+	classwork 
+ON 
+	time_table_day.id = classwork.time_table_day_key 
+AND 
+	time_table_day.lesson_date = SUBSTRING(NOW(), 1, 10) 
+INNER JOIN 
+	user_classwork 
+ON classwork.id = user_classwork.classwork_key 
+INNER 
+	JOIN user_inf 
+ON user_inf.id = user_classwork.user_key 
+INNER JOIN  
+	lesson_inf 
+ON 
+	lesson_inf.id = classwork.lesson_key 
+INNER JOIN 
+	timetable_inf 
+ON 
+	timetable_inf.id = time_table_day.timetable_key 
+AND 
+	user_classwork.user_work_status = 2
+;
+END$$
+delimiter ;
+
+CALL getLecturePermit(); SELECT * FROM tmp_lecture_permit;	
+
+#ポイントレート算出
+delimiter $$
+CREATE PROCEDURE getPointRate(in lessonKey int)
+BEGIN
+CREATE TEMPORARY TABLE 
+	tmp_point_rate AS
+SELECT
+    point_rate
+    ,students
+FROM
+    lesson_point_rate
+WHERE
+    lesson_point_rate.lesson_key = lessonKey
+;
+END$$
+delimiter ;
+
+CALL getPointRate('lesson_key'); SELECT * FROM tmp_point_rate;
+
+# 備品名リスト用クエリ
+delimiter $$
+CREATE PROCEDURE getCommodityNameList()
+BEGIN
+CREATE TEMPORARY TABLE 
+	tmp_commodity_name_list AS
+SELECT 
+	commodity_name
+	,selling_price
+	,id AS commodity_key 
+FROM 
+	commodity_inf;
+END$$
+delimiter ;
+
+CALL getCommodityNameList('lesson_key'); SELECT * FROM tmp_commodity_name_list;
+
