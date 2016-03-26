60533ee56bdd3cfec2a56de0a34c51dc71023504
増田貴志 <xellos00195@gmail.com>
2016-03-26 19:33:49 +0900

    商品販売承認一覧のJSファイル追加

---
+++ js/sellCommodityPermitList.js
@@ -0,0 +1,18 @@
+$(function(){
+	// use_pointの初期保存と差分保存
+	var target =  $("input[name='use_point']", $("#sellCommodityPermitList"));
+	// 初期値保存
+	target.each(function() {
+		$(this).focus(function() {
+			if(!$(this).attr("data-base_point")) {
+				$(this).attr("data-base_point", $(this).val());
+			}
+		})
+	});
+	// 差分保存
+	target.each(function() {
+		$(this).change(function() {
+			$(this).attr("data-diff_point", ($(this).val() - $(this).attr("data-base_point")));
+		})
+	});
+});
